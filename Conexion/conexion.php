6121f0e7acc91f5606bbaadd6c4a0127cf7c07c5
Leonardo062023 <137232212+Leonardo062023@users.noreply.github.com>
2023-06-20 18:11:07 -0500

    Update conexion.php
    asdasdasd

--- Conexion/conexion.php
+++ Conexion/conexion.php
@@ -3,7 +3,7 @@
 # Type="MYSQL"
 # HTTP="true"
 
-// $hostname_conexion = "localhost/orcl"; sdfgfhsafgsgsgdfhfshs
+// $hostname_conexion = "localhost/orcl"; sdfgfhsafgsgsgdfhfshsaaa
 $username_conexion = "sigiep1";
 $password_conexion = "123Marcelo";
 $oracle =  oci_connect($username_conexion, $password_conexion, $hostname_conexion, 'UTF8');
