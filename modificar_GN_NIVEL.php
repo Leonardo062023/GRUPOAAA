f61a17912ba51c05c894c0c5dba6ee204a202929
Leonardo062023 <137232212+Leonardo062023@users.noreply.github.com>
2023-06-20 18:00:10 -0500

    Leonardo

--- modificar_GN_NIVEL.php
+++ modificar_GN_NIVEL.php
@@ -1,6 +1,6 @@
 <?php require_once('Conexion/conexion.php');
 require_once ('./Conexion/conexion.php');
-# session_start();jaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+# session_start();jaaaaaaaaaaaaaaaaaaaaaaaaDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 $id = (($_GET["id"]));
   $sql = "SELECT    n.id_unico,
                     n.nombre,
